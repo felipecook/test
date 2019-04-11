13a6f66b700202580e11f2ee5a842d2f25df62e0
Felipe Cook <felipepcook@gmail.com>
2019-04-11 12:27:48 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/Test.java
@@ -0,0 +1,23 @@
+package edu.cnm.deepdive;
+
+
+
+class T {
+
+  int t = 20;
+
+  T() {
+    t = 40;
+  }
+}
+
+class Main {
+
+  public static void main(String args[]) {
+    T t1 = new T();
+    System.out.println(t1.t);
+    if (t1.e)
+  }
+
+
+}
